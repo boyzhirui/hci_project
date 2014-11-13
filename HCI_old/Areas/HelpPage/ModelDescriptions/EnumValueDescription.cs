e6580e2a6833ca972e45dd3bc15fb221b59258ec
Leo Yao <hyao4@ncsu.edu>
2014-11-13 18:21:01 -0500

    create a new HCI project (2)

---
+++ HCI_old/Areas/HelpPage/ModelDescriptions/EnumValueDescription.cs
@@ -0,0 +1,11 @@
+namespace HCI.Areas.HelpPage.ModelDescriptions
+{
+    public class EnumValueDescription
+    {
+        public string Documentation { get; set; }
+
+        public string Name { get; set; }
+
+        public string Value { get; set; }
+    }
+}
