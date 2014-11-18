8b0ac69e906151f0e08dcc18ec2ac01f0c9ae34b
Leo Yao <hyao4@ncsu.edu>
2014-11-18 16:58:59 -0500

    General Search Group

---
+++ HCI/Models/SearchGroupQueryModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HCI.Models
+{
+    public class SearchGroupQueryModel
+    {
+        public SearchGroupQueryModel()
+        {
+            QueryList = new List<SearchGroupQuery>();
+            GeneralSearchKeyword = string.Empty;
+            IsAdvancedSearch = false;
+        }
+
+        public Boolean IsAdvancedSearch { get; set; }
+        public string GeneralSearchKeyword { get; set; }
+        public IList<SearchGroupQuery> QueryList { get; set; }
+    }
+
+    public class SearchGroupQuery
+    {
+        public string SearchType { get; set; }
+        public string CombineType { get; set; }
+
+        public string SearchValue { get; set; }
+    }
+}
